eca1db076d039f6ed916521d270f1875787f8993
Lewis Wright <lewis@subora.com>
2014-01-18 13:11:34 +0000

    Added better handling of date fields.

--- Form/Extension/DateTypeExtension.php
+++ Form/Extension/DateTypeExtension.php
@@ -24,13 +24,37 @@ use Symfony\Component\Form\FormView;
 class DateTypeExtension extends AbstractTypeExtension
 {
     /**
+    * {@inheritdoc}
+    */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['input_wrapper_class'] = $options['input_wrapper_class'];
+        $view->vars['separator_wrapper_class'] = $options['separator_wrapper_class'];
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function finishView(FormView $view, FormInterface $form, array $options)
     {
-        if ('single_text' === $options['widget'] && isset($options['datepicker'])) {
-            $view->vars['datepicker'] = $options['datepicker'];
+        if ('single_text' === $options['widget']) {
+            if (isset($options['datepicker'])) {
+                $view->vars['datepicker'] = $options['datepicker'];
+            }
+            else {
+                $view->vars['date_placeholder_pattern'] = $options['format'];
+            }
         }
+
+        if (!empty($view->vars['date_pattern'])) {
+            $pattern = $view->vars['date_pattern'];
+
+            $view->vars['html_date_pattern'] = preg_replace('#(?<=^|\})([^\{\}]+?)(?=\{|$)#', '<span class="{{ date_separator_wrapper_class }}">$1</span>', $pattern);
+        }
+
+        $date_format = '%04d-%02d-%02d';
+        $view->vars['date_max'] = sprintf($date_format, max($options['years']), max($options['months']), max($options['days']));
+        $view->vars['date_min'] = sprintf($date_format, min($options['years']), min($options['months']), min($options['days']));
     }
 
     /**
@@ -39,7 +63,9 @@ class DateTypeExtension extends AbstractTypeExtension
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setOptional(array(
-            'datepicker'
+            'datepicker',
+            'input_wrapper_class',
+            'separator_wrapper_class'
         ));
     }
 
